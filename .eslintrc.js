06d3b612fe83279c011db1af1061766a121dc8c2
Rogério Munhoz <roz@rjmunhoz.me>
2020-08-23 15:06:39 -0300

    :wrench: Fix linter config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,17 @@
 module.exports = {
-  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
-  extends: ["standard-with-typescript"],
+  env: {
+    es2020: true,
+    node: true
+  },
+  extends: ['standard'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-    sourceType: "module", // Allows for the use of imports,
-    project: "./tsconfig.json",
+    ecmaVersion: 12,
+    sourceType: 'module'
   },
+  plugins: ['@typescript-eslint'],
   rules: {
-    "@typescript-eslint/prefer-optional-chain": "warn",
-    "@typescript-eslint/strict-boolean-expressions": "off",
-    "@typescript-eslint/consistent-type-definitions": ["error", "type"],
-    "@typescript-eslint/no-inferrable-types": "warn",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/require-await": "off",
-    "@typescript-eslint/no-var-requires": "off",
-    "array-bracket-spacing": ["error", "always"],
-    "computed-property-spacing": "off",
-    "no-warning-comments": "error",
-  },
-};
+    'no-unused-vars': 'off',
+    'no-useless-constructor': 'off'
+  }
+}
